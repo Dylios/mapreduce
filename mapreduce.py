16d9974e1ba697b133d54105cf0e2db6cfe6069e
Dylios <thomaslorillon@gmail.com>
2016-10-07 11:08:53 +0200

    final commit

--- mapreduce.py
+++ mapreduce.py
@@ -6,25 +6,24 @@ import re
 def map():
     tabmap = []
     fichier = input("Veuillez renseigner le fichier à analyser:\n")
+    """Ouverture du fichier texte à analyser"""
     with open(fichier, 'r') as f:
+        """ Recherche de tous les mots présent dans le texte grâce aux expressions régulière"""
         m = re.findall('[A-Za-z]{0,}\w+', f.read())
-        print('Votre fichier contient:', len(m))
+        print('Votre fichier contient:', len(m), "mots.")
         os.system("pause")
         for i in range (0, len(m)):
             tabmap.append([0] * 2)
-            #print(tabmap)
-            #os.system("pause")
+            """"On remplit un tableau avec chaque mot (comme clé) et la valeur 1 associée"""
             tabmap[i][0] = m[i]
             tabmap[i][1] = 1
             print('{0} \t\t {1}'.format(tabmap[i][0], tabmap[i][1]))
-            # tabmap[i][0] = m[i]
-            # tabmap[i][1] = '1'
-            # print(tabmap[i][0]+"=======>"+tabmap[i][1]+"\n")
     f.close()
     return tabmap
 
 def reduce(word, tabword):
     tabreduce = []
+    """ Création d'un nouveau tableau associant la clé (le mot) avec son nombre d'occurence dans le texte"""
     tabreduce.append(word)
     tabreduce.append(tabword.count(1))
     return tabreduce
@@ -34,6 +33,7 @@ def main():
     tabmap = map()
     print( '\n \n' )
     print('***** REDUCE *****\n')
+    """Création d'une clé et du nombre d'occurance pour tester la fonction reduce"""
     word = "Peter"
     tabword = []
     for i in range (0, 6):
